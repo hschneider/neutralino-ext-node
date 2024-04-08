e3ccf316ea2a1f45df90d84dbd72cca1cd185c19
Harald Schneider <h_schneider@marketmix.com>
2024-04-08 10:50:04 +0200

    Update run.cmd

--- extensions/node/run.cmd
+++ extensions/node/run.cmd
@@ -3,4 +3,4 @@ set NODE_VIRTUAL_ENV=%1\extensions\node\_runtime\nodejs-win
 set NODE_PATH=%NODE_VIRTUAL_ENV%\node_modules
 set NPM_CONFIG_PREFIX=%NODE_VIRTUAL_ENV%
 set PATH=%NODE_VIRTUAL_ENV%\Scripts;%NODE_PATH%;%NODE_PATH%\bin;%NODE_VIRTUAL_ENV%\node-modules;%PATH%
-node.exe --inspect %1\extensions\node\main.js
+%NODE_VIRTUAL_ENV%\Scripts\node.exe --inspect %1\extensions\node\main.js
